f72a9b703e683667a32bae46de5371f710cc4022
Deadeye5589 <Deadeye5589@hotmail.com>
2023-01-09 08:58:40 +0100

    Fixed Stack Overflow with ESP32 lib >1.0.6
    Vereinfachung der Funktion getNTPTimestamp
    UDP Objekt als lokale Instanz der Funktion verhinder Stack Overflow

--- SW/time_ntp.cpp
+++ SW/time_ntp.cpp
@@ -12,10 +12,9 @@ code for time conversion based on http://stackoverflow.com/
 
 // NTP specifics
 IPAddress timeServer(134,130,4,17); // time.nist.gov NTP server
-WiFiUDP udp;  // A UDP instance to let us send and receive packets over UDP
+
 unsigned int ntpPort = 2390;          // local port to listen for UDP packets
 const int NTP_PACKET_SIZE = 48;       // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE];  //buffer to hold incoming and outgoing packets
 
 // ntp flag
 bool bNTPStarted=false;
@@ -45,9 +44,33 @@ unsigned int date_time_to_epoch(date_time_t* date_time)
 unsigned long getNTPTimestamp()
 {
   unsigned long ulSecs2000;
+  WiFiUDP udp;  // A UDP instance to let us send and receive packets over UDP
+  byte packetBuffer[ NTP_PACKET_SIZE];  //buffer to hold incoming and outgoing packets
   
   udp.begin(ntpPort);
-  sendNTPpacket(timeServer); // send an NTP packet to a time server
+  //sendNTPpacket(timeServer); // send an NTP packet to a time server
+
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(timeServer, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+
   delay(1000);    // wait to see if a reply is available
   int cb=0, repeat=0; 
   while(!cb && repeat<20)  // try for 2 sec
@@ -79,31 +102,6 @@ unsigned long getNTPTimestamp()
   return(ulSecs2000);
 }
 
-
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
-}
 
 
 void epoch_to_date_time(date_time_t* date_time,unsigned int epoch)
